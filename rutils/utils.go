848b8b541b62e2c600fe64a65a9ff31fa5531df6
Pierre-Monier <p.monier96@gmail.com>
2020-09-12 11:42:34 +0200

    Update music (download it)

--- rutils/utils.go
+++ rutils/utils.go
@@ -1,27 +1,69 @@
 package rutils
 
 import (
+	"encoding/json"
+	"net/http"
 	"strconv"
 )
 
+// GetStuffsFromApi return data from api
+func GetStuffsFromApi(url string) ([]map[string]interface{}, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	// parse resposne
 
+	var body []map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+// GetFilesFromDropbox return files (music file) from dropbox
+func GetFilesFromDropbox(url string) (*http.Response, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	return res, nil
+}
+
+// FormatUserChoice return a clean playlist selection
 func FormatUserChoice(playlists []map[string]interface{}) string {
 	i := 1
 	res := ""
 	for _, v := range playlists {
-		res += strconv.Itoa(i)+")"+v["name"].(string)+"\n"
+		res += strconv.Itoa(i) + ")" + v["name"].(string) + "\n"
 		i++
-	}	
+	}
 	return res
 }
 
+// ValideUserResponse check if the user response (of the playlist choice) is correct
 func ValideUserResponse(userResponse string, pLenght int64) bool {
-	nbr, err := strconv.ParseInt(userResponse, 10, 32)
+	nbr, err := strconv.ParseInt(userResponse, 10, 8)
 	if err != nil {
 		return false
-	}else if nbr <= pLenght && nbr > 0 {
+	} else if nbr <= pLenght && nbr > 0 {
 		return true
-	}else {
+	} else {
 		return false
+	}
+}
+
+// GetPlaylistDir return a playlist chosen dir
+func GetPlaylistDir(radioDir string, playlists []map[string]interface{}, userChoice string) string {
+	i, err := strconv.ParseInt(userChoice, 10, 8)
+	if err != nil {
+		return ""
+	} else {
+		return radioDir + playlists[i-1]["name"].(string)
 	}
 }
--- src/radio.go
+++ src/radio.go
@@ -1,71 +1,52 @@
 package main
 
-import(
-	"fmt"    
-	"net/http"
-	"encoding/json"
-	"os"
+import (
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"radio/rutils"
 )
 
-func main(){
+func main() {
 	log.SetPrefix("radio: ")
 	log.SetFlags(0)
 	radioDir := "/home/yoon/Radio/"
 
-	playlists, err := getPlaylistFromDropbox(radioDir)
+	playlists, err := rutils.GetStuffsFromApi("https://api-f-fstreaming.p-monier.fr/api/playlists")
+	// playlists, err := getPlaylistFromDropbox(radioDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	createDirForPlaylist(radioDir, playlists)
 	fChoices := rutils.FormatUserChoice(playlists)
 	userChoice := getUserChoice(fChoices, len(playlists))
-	fmt.Println(userChoice)
+	playlistDir := rutils.GetPlaylistDir(radioDir, playlists, userChoice)
+	updateChoosenPlaylist(playlistDir, userChoice)
 }
 
-func getPlaylistFromDropbox(radioDir string) ([]map[string]interface{}, error) {
-	// get all the playlist from api
-	res, err := http.Get("https://api-f-fstreaming.p-monier.fr/api/playlists")
-	if err != nil {
-		fmt.Println("Something got wrong in the api call")
-	}
-	defer res.Body.Close()
-	// parse resposne 
-
-	var body []map[string]interface {}
-	err = json.NewDecoder(res.Body).Decode(&body)
-	if err != nil {
-		fmt.Println("Something got wrong in the api call")
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func createDirForPlaylist(radioDir string, playlists []map[string]interface{}){
-    // test if their is an existing dir for each playlist, if not, create one
+func createDirForPlaylist(radioDir string, playlists []map[string]interface{}) {
+	// test if their is an existing dir for each playlist, if not, create one
 	for _, v := range playlists {
-		filepath := radioDir+v["name"].(string)
-		noDir := false
+		filepath := radioDir + v["name"].(string)
+		noDir := true
 		fileinfo, err := os.Stat(filepath)
 		// if no dir, create one, if file is a file (not a dir), remove file and create a dir
 		if os.IsNotExist(err) {
-			noDir = true
-		}else if !fileinfo.IsDir() {
+			fmt.Println("Create the playlist " + v["name"].(string))
+		} else if !fileinfo.IsDir() {
 			os.Remove(filepath)
-			noDir = true
+		} else if fileinfo.IsDir() {
+			noDir = false
 		}
-
 		if noDir {
-			os.Mkdir(filepath, 0664)
+			os.Mkdir(filepath, 0774)
 		}
 	}
 }
 
 func getUserChoice(fChoices string, pLength int) string {
-	fmt.Println(pLength)
-	fmt.Println("Select the playlist \n"+fChoices)
+	fmt.Println("Select the playlist \n" + fChoices)
 	match := false
 	var userResponse string
 	for !match {
@@ -73,4 +54,32 @@ func getUserChoice(fChoices string, pLength int) string {
 		match = rutils.ValideUserResponse(userResponse, int64(pLength))
 	}
 	return userResponse
+}
+
+func updateChoosenPlaylist(playlistDir string, userChoice string) error {
+	fmt.Println("Updating the playlist...")
+
+	musics, err := rutils.GetStuffsFromApi("https://api-f-fstreaming.p-monier.fr/api/playlists/" + userChoice)
+	if err == nil {
+		for _, v := range musics {
+			filepath := playlistDir + "/" + v["name"].(string)
+			_, err := os.Stat(filepath)
+			if os.IsNotExist(err) {
+				fmt.Println("Uploading: " + v["name"].(string))
+				music, err := rutils.GetFilesFromDropbox(v["path"].(string))
+				if err != nil {
+					fmt.Println(err)
+				}
+				// Create the file
+				out, err := os.Create(filepath)
+				if err != nil {
+					fmt.Println(err)
+				}
+				// write the body resp into it
+				_, err = io.Copy(out, music.Body)
+			}
+		}
+		fmt.Println("Updated")
+	}
+	return err
 }
