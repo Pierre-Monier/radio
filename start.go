b5195eca97a9269ea8497335463fcf514f2bf6fc
Pierre-Monier <p.monier96@gmail.com>
2020-09-14 17:49:42 +0200

    Add basic test mp3 playing

--- start.go
+++ start.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func start(playlistFilepath string) {
-	fmt.Println(playlistFilepath)
+	f, err := os.Open(playlistFilepath + "/oaka-Close 2 U.mp3")
+	if err != nil {
+		fmt.Println(err)
+	}
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Playing")
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+	<-done
+	// keep the program running forever
+	// select {}
 }
